b933e8ffada2f97b65ade533abc80abdbe1c5533
4uvak404 <roma02696@gmail.com>
2022-08-30 18:51:23 +0300

    Добавил класс Snake, с полями и свойствами

---
+++ Snake.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSnake
+{
+    internal class Snake
+    {
+        private int mapWidth, mapHeight;
+        private int factLenght, wantedLenght;
+        private Direction direction;
+        private Color bodyColor;
+        private List<Point> body = new List<Point>();
+
+        public int MapWidth
+        {
+            get { return mapWidth; }
+            set
+            {
+                if (value > 1)
+                {
+                    mapWidth = value;
+                }
+            }
+        }
+        public int MapHeight
+        {
+            get { return mapHeight; }
+            set
+            {
+                if (value > 1)
+                {
+                    mapHeight = value;
+                }
+            }
+        }
+        public int FactLenght
+        {
+            get { return factLenght; }
+            private set
+            {
+                if (value > 0)
+                {
+                    factLenght = value;
+                }
+            }
+        }
+        public int WantedLenght
+        {
+            get { return wantedLenght; }
+            set
+            {
+                if (value > 0)
+                {
+                    wantedLenght = value;
+                }
+            }
+        }
+        public Direction Direction
+        {
+            get { return direction; }
+            set
+            {
+                switch (value)
+                {
+                    case Direction.Up:
+                        if (direction != Direction.Down)
+                        {
+                            direction = value;
+                        }
+                        break;
+                    case Direction.Right:
+                        if (direction != Direction.Left)
+                        {
+                            direction = value;
+                        }
+                        break;
+                    case Direction.Down:
+                        if (direction != Direction.Right)
+                        {
+                            direction = value;
+                        }
+                        break;
+                    case Direction.Left:
+                        if (direction != Direction.Left)
+                        {
+                            direction = value;
+                        }
+                        break;
+                }
+            }
+        }
+
+    }
+}
