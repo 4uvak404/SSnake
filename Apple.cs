81ae1581f1e59a0bb8ba99da3d5b9f041e9ed4b7
4uvak404 <roma02696@gmail.com>
2022-09-04 02:59:11 +0300

    Добавил класс яблоко с необходимыми свойствами

---
+++ Apple.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSnake
+{
+    internal class Apple
+    {
+        private int mapHeight;
+        private int mapWidth;
+        private Bitmap picture;
+        private Point cords;
+        private Random rnd;
+
+        public int MapHeight
+        {
+            get { return mapHeight; }
+            set
+            {
+                if (value > 1)
+                {
+                    mapHeight = value;
+                }
+            }
+        }
+        public int MapWidth
+        {
+            get { return mapWidth; }
+            set
+            {
+                if (value > 1)
+                {
+                    mapWidth = value;
+                }
+            }
+        }
+        public Bitmap Picture
+        {
+            get { return picture; }
+            set { picture = value; }
+        }
+        public Point Coordinates
+        {
+            get { return cords; }
+            set { cords = value; }
+        }
+    }
+}
