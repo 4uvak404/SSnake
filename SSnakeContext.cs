a80259c3d4aa4d9caa5e4b6e9b9b9109bce11631
4uvak404 <roma02696@gmail.com>
2022-10-01 15:11:37 +0300

    Добавлен класс SSnakeContext
    Класс контекста для обращения к базе данных с рекордами

---
+++ SSnakeContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SSnake
+{
+    internal class SSnakeContext : DbContext
+    {
+        public DbSet<Record> Records { get; set; } = null!;
+        public SSnakeContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=SSnakeDB.db");
+        }
+    }
+}
