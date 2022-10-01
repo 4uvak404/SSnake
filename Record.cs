6a59af5254ba034e677027a65ed0f59e66735b6c
4uvak404 <roma02696@gmail.com>
2022-10-01 14:44:15 +0300

    Добавлен класс Record
    Он орпеделяет структуру таблицы с рекордами в базе данных при помощи Entity Framework

---
+++ Record.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSnake
+{
+    internal class Record
+    {
+        public int Id { get; set; }
+        public string? NickName { get; set; }
+        public int Score { get; set; }
+        public DateTime TimePassed { get; set; }
+        public int MapWidth { get; set; }
+        public int MapHeight { get; set; }
+        public string? SpeedName { get; set; }
+        public double SpeedValue { get; set; }
+        public DateTime RecordDateTime { get; set; }
+        public int SnakeLength { get; set; }
+
+    }
+}
