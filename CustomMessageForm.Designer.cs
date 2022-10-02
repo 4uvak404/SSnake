7545c1a79017f3bb9a965273b39d77680ef3a4d6
4uvak404 <roma02696@gmail.com>
2022-10-02 13:39:46 +0300

    Добавлена форма кастомного диалогового окна
    Форма содержит в себе:
    Лейбл в которой через свойство можно записать любой текст.
    Текстбокс с подписью "Никнейм" в котором можно через свойство написать или считать текст.
    Две кнопки "Да" и "Нет" которые присваивают соответствующий DialogResult форме и закрывают её.

---
+++ CustomMessageForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace SSnake
+{
+    partial class CustomMessageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelMainText = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.buttonNo = new System.Windows.Forms.Button();
+            this.buttonYes = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelMainText
+            // 
+            this.labelMainText.Location = new System.Drawing.Point(36, 23);
+            this.labelMainText.Name = "labelMainText";
+            this.labelMainText.Size = new System.Drawing.Size(324, 80);
+            this.labelMainText.TabIndex = 0;
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(141, 125);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(198, 27);
+            this.textBox.TabIndex = 1;
+            // 
+            // buttonNo
+            // 
+            this.buttonNo.Location = new System.Drawing.Point(59, 171);
+            this.buttonNo.Name = "buttonNo";
+            this.buttonNo.Size = new System.Drawing.Size(65, 36);
+            this.buttonNo.TabIndex = 2;
+            this.buttonNo.Text = "Нет";
+            this.buttonNo.UseVisualStyleBackColor = true;
+            this.buttonNo.Click += new System.EventHandler(this.buttonNo_Click);
+            // 
+            // buttonYes
+            // 
+            this.buttonYes.Location = new System.Drawing.Point(130, 171);
+            this.buttonYes.Name = "buttonYes";
+            this.buttonYes.Size = new System.Drawing.Size(209, 36);
+            this.buttonYes.TabIndex = 3;
+            this.buttonYes.Text = "Да";
+            this.buttonYes.UseVisualStyleBackColor = true;
+            this.buttonYes.Click += new System.EventHandler(this.buttonYes_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Никнейм:";
+            // 
+            // CustomMessageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 230);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonYes);
+            this.Controls.Add(this.buttonNo);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.labelMainText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "CustomMessageForm";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label labelMainText;
+        private TextBox textBox;
+        private Button buttonNo;
+        private Button buttonYes;
+        private Label label1;
+    }
+}
---
+++ CustomMessageForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SSnake
+{
+    internal partial class CustomMessageForm : Form
+    {
+        public CustomMessageForm()
+        {
+            InitializeComponent();
+        }
+        internal string MainText
+        {
+            get { return labelMainText.Text; }
+            set { labelMainText.Text = value; }
+        }
+        internal string TextBoxText
+        {
+            get { return textBox.Text; }
+            set { textBox.Text = value; }
+        }
+
+        private void buttonNo_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.No;
+            Close();
+        }
+
+        private void buttonYes_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Yes;
+            Close();
+        }
+    }
+}
