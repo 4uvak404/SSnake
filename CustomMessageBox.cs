51e18824f0f4c091830298cc9f4c9f3a71b36279
4uvak404 <roma02696@gmail.com>
2022-10-02 13:42:37 +0300

    Добавлен статический класс CustomMessageBox
    Данный класс при помощи метода Show выводит диалоговое окно основанное на CustomMessageForm

---
+++ CustomMessageBox.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSnake
+{
+    internal static class CustomMessageBox
+    {
+        public static void Show()
+        {
+            using (CustomMessageForm form = new CustomMessageForm())
+            {
+                form.ShowDialog();
+            }
+        }
+        public static string? Show(string header, string mainText, string defaultText)
+        {
+            using (CustomMessageForm form = new CustomMessageForm())
+            {
+                form.Text = header;
+                form.MainText = mainText;
+                form.TextBoxText = defaultText;
+                if (form.ShowDialog() == DialogResult.Yes)
+                {
+                    return form.TextBoxText;
+                }
+                else return null;
+            }
+        }
+    }
+}
